5f96d7f1a1b7dc70298b96b011328e6d6b940f1a
vittorfraga <110206437+vittorfraga@users.noreply.github.com>
2023-01-23 14:21:16 -0300

    first commit

---
+++ src/entities/Video.ts
@@ -0,0 +1,24 @@
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  ManyToOne,
+  JoinColumn,
+} from "typeorm";
+import { Room } from "./Room";
+
+@Entity("videos")
+export class Video {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({ type: "text" })
+  title: string;
+
+  @Column({ type: "text" })
+  url: string;
+
+  @ManyToOne(() => Room, (room) => room.videos)
+  @JoinColumn({ name: "room_id" })
+  room: Room;
+}
---
+++ src/repositores/VideoRepository.ts
@@ -0,0 +1,5 @@
+import { AppDataSource } from "../data-source";
+
+import { Video } from "../entities/Video";
+
+export const videoRepository = AppDataSource.getRepository(Video);
